ee3c544f2db687471f4075f9c3567da0938bf3af
王子超 <2758213723@qq.com>
2025-06-03 00:11:10 +0800

    feat: Nest模块之间的循环依赖

---
+++ module-test/src/ccc.service.ts
@@ -0,0 +1,17 @@
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { DddService } from './ddd.service';
+
+@Injectable()
+export class CccService {
+  constructor(
+    @Inject(forwardRef(() => DddService)) private dddService: DddService,
+  ) {}
+
+  ccc() {
+    return 'ccc';
+  }
+
+  eee() {
+    return this.dddService.ddd() + 'eee';
+  }
+}
